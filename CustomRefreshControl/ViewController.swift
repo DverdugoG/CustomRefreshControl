2c5679335ebd06b923e704a866ce1340b3528e51
Shankar B S <getshankarsait@gmail.com>
2017-03-26 01:22:48 +0530

    Initial Commit

---
+++ CustomRefreshControl/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CustomRefreshControl
+//
+//  Created by Shankar B S on 26/03/17.
+//  Copyright © 2017 SliCode. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
